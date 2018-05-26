cf69426bbd161393b0f4f10bf60f9c26a106b0d7
Jeen Broekstra <jeen.broekstra@gmail.com>
2018-05-27 09:32:49 +1000

    eclipse/rdf4j-storage#67 tweak test suite setup [wip]

--- shacl/src/main/java/org/eclipse/rdf4j/shacl/manifest/AbstractSHACLTest.java
+++ shacl/src/main/java/org/eclipse/rdf4j/shacl/manifest/AbstractSHACLTest.java
@@ -7,13 +7,19 @@
  *******************************************************************************/
 package org.eclipse.rdf4j.shacl.manifest;
 
+import java.nio.channels.ShutdownChannelGroupException;
+
 import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.Resource;
+import org.eclipse.rdf4j.model.vocabulary.RDF;
+import org.eclipse.rdf4j.model.vocabulary.SHACL;
 import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.repository.RepositoryException;
 import org.eclipse.rdf4j.repository.sail.SailRepository;
 import org.eclipse.rdf4j.sail.Sail;
 import org.eclipse.rdf4j.sail.SailException;
+import org.eclipse.rdf4j.sail.shacl.ShaclSail;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -80,20 +86,24 @@ public abstract class AbstractSHACLTest extends TestCase {
 	protected Repository createRepository(Model shapesGraph)
 		throws Exception
 	{
-		Repository repo = new SailRepository(newSail(shapesGraph));
+		Repository repo = new SailRepository(newSail());
 		repo.initialize();
-		RepositoryConnection con = repo.getConnection();
-		try {
-			con.clear();
-			con.clearNamespaces();
-		}
-		finally {
-			con.close();
+
+		try (RepositoryConnection conn = repo.getConnection()) {
+			conn.clear();
+			conn.clearNamespaces();			
+			for (Resource subj : shapesGraph.filter(null, RDF.TYPE, SHACL.NODE_SHAPE).subjects()) {
+				shapesGraph.add(subj, RDF.TYPE, SHACL.SHAPE);
+			}
+			for (Resource subj : shapesGraph.filter(null, RDF.TYPE, SHACL.PROPERTY_SHAPE).subjects()) {
+				shapesGraph.add(subj, RDF.TYPE, SHACL.SHAPE);
+			}
+			conn.add(shapesGraph, ShaclSail.SHACL_GRAPH);
 		}
 		return repo;
 	}
 
-	protected abstract Sail newSail(Model shapesGraph);
+	protected abstract Sail newSail();
 
 	@Override
 	public void tearDown()
